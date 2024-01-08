2ff2d5ceee989dc0b465b4020771fd7fee9aae65
amora7741 <mora7741@gmail.com>
2024-01-08 15:20:33 -0800

    Adjust colors

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const cells = document.querySelectorAll(".cell");
 
-cells.forEach((cell) => {
-    cell.addEventListener('click', () => {
-        cell.textContent = '';
-    });
-});
+function Gameboard() {
+    
+}
